d584e74adbfc01a461700f5fec4c963886824874
radithya027 <rizkyradithya027@gmail.com@gmail.com>
2024-12-03 16:55:38 +0700

    [RR]slicing dashboard user

---
+++ views/pages/dashoardUser/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kelas Sore</title>
+    <link rel="stylesheet" href="assets/css/dashboard/dashbod.css"> <!-- Include CSS File -->
+</head>
+<body>
+<?php 
+include('views/pages/dashoardUser/dashboard.php'); 
+include('views/pages/dashoardUser/content.php'); 
+?>
+
+</body>
+</html>
