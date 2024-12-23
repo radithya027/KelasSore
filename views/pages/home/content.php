1ddce0fa5be7ef09feb93fb80650e3d0f0d07f35
radithya027 <rizkyradithya027@gmail.com@gmail.com>
2024-12-23 09:45:13 +0700

    [RR] add category class

--- views/pages/home/content.php
+++ views/pages/home/content.php
@@ -5,25 +5,29 @@
             <h2>Belajar dari Profesional</h2>
             <p>Raih pengetahuan langsung dari para ahli dengan pengalaman nyata. Pelajari wawasan, tips, dan strategi yang digunakan oleh para pemimpin industri.</p>
         </div>
-        <div class="divider"></div>
+        <div class="divider" style="height: 1px; background-color: #ccc; margin: 20px 0;"></div>
+
         
         <div class="benefit-item fade-up stagger-delay-2">
             <h2>Jam Belajar yang Menyesuaikan</h2>
             <p>Jika Anda memilih private course, jam belajar dapat disesuaikan dengan jadwal Anda. Untuk kelas reguler, jam belajar mengikuti jadwal yang telah ditentukan.</p>
         </div>
-        <div class="divider"></div>
+       <div class="divider" style="height: 1px; background-color: #ccc; margin: 20px 0;"></div>
+
 
         <div class="benefit-item fade-up stagger-delay-3">
             <h2>Pengalaman Belajar Interaktif</h2>
             <p>Nikmati kuis, tugas, dan diskusi dengan peserta lain untuk memperdalam pemahaman dan meningkatkan hasil belajar Anda.</p>
         </div>
-        <div class="divider"></div>
+       <div class="divider" style="height: 1px; background-color: #ccc; margin: 20px 0;"></div>
+
 
         <div class="benefit-item fade-up stagger-delay-5">
             <h2>Beragam Pilihan Kursus</h2>
             <p>Akses berbagai kursus di berbagai bidang, mulai dari teknologi hingga pengembangan diri dan banyak lagi.</p>
         </div>
-        <div class="divider"></div>
+       <div class="divider" style="height: 1px; background-color: #ccc; margin: 20px 0;"></div>
+
     </section>
 
     <section class="benefitwithus fade-up">
@@ -32,7 +36,8 @@
             <p>Temukan alasan mengapa kursus di kelas sore adalah pilihan yang tepat untuk mendukung perjalanan belajar Anda.</p>
             <a href="#" class="start-learn-btn">Mulai Belajar Sekarang</a>
         </div>
-        <div class="divider"></div>
+       <div class="divider" style="height: 1px; background-color: #ccc; margin: 20px 0;"></div>
+
         <img src="../../../../assets/images/homecontent.svg" alt="Manfaat Kursus" class="benefit-image slide-in-left">
     </section>
 </div>
